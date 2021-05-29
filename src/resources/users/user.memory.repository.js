5bbed726b8035221da00d12ea6a9e23c9c779bd1
NebelwerferT <NebelwerferT9@gmail.com>
2021-05-29 19:06:56 +0400

    doc: user.memory.repository

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -2,12 +2,32 @@ const User = require("./user.model");
 
 const repo = [new User(), new User(), new User(), new User(), new User(), new User()];
 
+/**
+ * Get all users from the repository
+ * @returns {Promise<Array<User>>} a promise object representing an array of users
+ */
 const getAll = async () => repo;
 
+/**
+ * Get an user by id from the repository
+ * @param {string} id id of the requested user
+ * @returns {Promise<User>} a promise object representing an user
+ */
 const getById = async (id) => repo.filter(user => user.id === id)[0];
 
+/**
+ * Create a new user in the repository
+ * @param {object} reqBody an object from the request body with an user structure
+ * @returns {Promise<User>} a promise object representing a created user
+ */
 const createUser = async (reqBody) => repo[repo.push(new User(reqBody)) - 1];
 
+/**
+ * Sends data according to which the user with the specified id will be updated.
+ * @param {string} id id of the user to update
+ * @param {string} name new name value for the user being updated
+ * @returns {Promise<User>} a promise object representing an updated user
+ */
 const updateById = async (id, name) => {
   const updatedUser = repo[repo.indexOf(repo.filter(user => user.id === id)[0])];
   if (updatedUser !== undefined) {
@@ -16,6 +36,11 @@ const updateById = async (id, name) => {
   return updatedUser;
 };
 
+/**
+ * Delete an user by id from the repository
+ * @param {string} id id of the user to remove
+ * @returns {Promise<User>} a promise object representing a deleted user
+ */
 const deleteById =  async (id) => {
   const deletedUser = repo.filter(user => user.id === id)[0];
   if (deletedUser !== undefined) {repo.splice(repo.indexOf(deletedUser), 1);}
