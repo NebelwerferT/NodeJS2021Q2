5e5a07e98e6225a29d84bafb879be8756534a1ec
NebelwerferT <NebelwerferT9@gmail.com>
2021-05-30 16:29:26 +0400

    refactor: replace double slashes in paths

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const swaggerUI = require('swagger-ui-express');
 const path = require('path');
 const YAML = require('yamljs');
-const fs = require("fs");
+const {access, constants} = require("fs");
 const userRouter = require('./resources/users/user.router');
 const boardRouter = require('./resources/boards/board.router');
 const taskRouter = require('./resources/tasks/task.router');
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use('/docs', (req, res, next) => {
-  fs.access(`${__dirname  }\\..\\out\\index.html`, fs.constants.R_OK, (err) => {
+  access(`${__dirname  }/../out/index.html`, constants.R_OK, (err) => {
     if (err) {
       res.status(404).send("Not Found. Try to execute the npm run doc script through the terminal in the project directory.");
     }
