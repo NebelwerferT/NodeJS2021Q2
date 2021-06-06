cc0402504e966d35fe6dc29889d4afef7bb2ce7d
NebelwerferT <NebelwerferT9@gmail.com>
2021-06-06 23:10:13 +0400

    update: add logs paths

--- src/common/config.ts
+++ src/common/config.ts
@@ -8,6 +8,13 @@ dotenv.config({
 const { PORT, NODE_ENV, MONGO_CONNECTION_STRING, JWT_SECRET_KEY } = process.env;
 const AUTH_MODE = process.env['AUTH_MODE'] === 'true';
 
+const LOG_DIR = 'logs';
+const REQ_LOG = `${LOG_DIR}/log_info.txt`;
+const ERR_LOG = `${LOG_DIR}/log_errors.txt`;
+const UNP_LOG = `${LOG_DIR}/log_unexpected_errors.txt`;
+
+export { LOG_DIR, REQ_LOG, ERR_LOG, UNP_LOG };
+
 export {
   PORT,
   NODE_ENV,
