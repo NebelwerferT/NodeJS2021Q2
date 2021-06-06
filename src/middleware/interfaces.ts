aa1f4c560cb2ee9f5b04cbf5d4bdd2cb8207c7c6
NebelwerferT <NebelwerferT9@gmail.com>
2021-06-06 23:05:32 +0400

    update: add interfaces for logging

---
+++ src/middleware/interfaces.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from "express";
+import { StatusCodes, ReasonPhrases } from 'http-status-codes';
+
+export interface ILogErr extends Error {
+    date?: Date,
+    reqres: IReqRes;
+    statusCode: number;
+    msg: string;
+    stack?: string;
+};
+
+export interface IReqRes {
+    req: Request,
+    res: Response,
+};
+
+export interface IUnexpErr extends Error {
+    date: Date,
+    statusCode: number;
+    msg: string;
+};
+
+export interface ILogInfo {
+    date: Date,
+    req: Request,
+    res: Response,
+    time: number;
+};
+
+export class UnexpErr extends Error implements IUnexpErr {
+    date: Date;
+    statusCode: number;
+    msg: string;
+    constructor(
+        date: Date,
+        statusCode: number,
+        msg: string
+    ) {
+        super();
+        this.date = date;
+        this.statusCode = statusCode;
+        this.msg = msg;
+    }
+};
+
+export class LogErr extends Error implements ILogErr {
+    reqres: IReqRes;
+    statusCode: number;
+    msg: string;
+    stack?: string;
+    date?: Date;
+
+    constructor(
+        reqres: IReqRes,
+        statusCode: number = StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: string = ReasonPhrases.INTERNAL_SERVER_ERROR,
+        stack?: string,
+        date?: Date,
+    ) {
+        super();
+        this.date = date;
+        this.reqres = reqres;
+        this.statusCode = statusCode;
+        this.msg = msg;
+        this.stack = stack;
+    }
+};
